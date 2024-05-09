d2eab49b4d80070bad25bb9d74f08a22620e41a9
anjalikesherwani <122610979+anjalikesherwani@users.noreply.github.com>
2024-05-09 16:37:00 +0530

    add_button

--- lib/login.dart
+++ lib/login.dart
@@ -52,6 +52,47 @@ class _MyLoginState extends State<MyLogin> {
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10))),
                   ),
+                  SizedBox(height: 40,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Text('Sign In',
+                    style: TextStyle(
+                     color:  Color(0xff4c505b),
+                      fontSize: 27,
+                    fontWeight: FontWeight.w700),),
+                    CircleAvatar(radius:30,
+                    backgroundColor: Color(0xff4c505b),
+                    child: IconButton(
+                      color: Colors.white,
+                      onPressed: () {},
+                      icon:Icon(Icons.arrow_forward),
+                    ),
+                    )
+                    ],
+                  ),
+                    SizedBox(height: 40,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text("Sign Up",style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        fontSize: 18 ,
+                        color: Color(0xff6d6e70)
+                        ),
+                        )),
+                        TextButton(
+                      onPressed: () {},
+                      child: Text("Forgot Password",
+                      style: TextStyle(
+                        // backgroundColor: Color(0xff4c505b),
+                        decoration: TextDecoration.underline,
+                        fontSize: 18 ,
+                        color: Colors.white,
+                        ),
+                        )),
+                  ],)
                 ],),
             ))
             ],
